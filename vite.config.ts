216d8c35b958169f1177a1bb00fbca2970e4389a
Hari Krishna Salver <harikrishna.salver@gmail.com>
2026-05-24 21:36:10 +0530

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    base: "/harikrishna-salver/",
+    base: "/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
