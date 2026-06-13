0c1b393329ebf08a50965223fe1add31b632399b
Hari Krishna Salver <harikrishna.salver@gmail.com>
2026-06-13 20:45:30 +0530

    hash router support

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,7 +4,7 @@ import {
   Route,
   useNavigate,
   useParams,
-  BrowserRouter,
+  HashRouter,
   useLocation,
 } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
@@ -70,11 +70,11 @@ export function AppRoutes() {
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <div className="min-h-screen bg-[#0f1113] text-[#e2e8f0] selection:bg-indigo-500/30 selection:text-indigo-200">
         <AppRoutes />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
